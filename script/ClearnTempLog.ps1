a8328c94bfb030c46b300a4dbd6b2d643f6bb26b
panda8 <45614227+pandasec888@users.noreply.github.com>
2023-10-31 17:08:13 +0800

    Delete script directory

--- script/ClearnTempLog.ps1
+++
@@ -1,5 +0,0 @@
-function ClearnTempLog {
-    $TempLogPath = $env:temp
-    write-host $TempLogPath
-    Remove-Item $TempLogPath"\*" -Recurse -Force 2>&1 | Out-Null
-}
--- script/KillEvenlogService.ps1
+++
@@ -1,5 +0,0 @@
-function KillEvenlogService {
-    # Kill Eventlog Service
-    $EventlogSvchostPID = Get-WmiObject -Class win32_service -Filter "name = 'eventlog'" | select -exp ProcessId
-    taskkill /F /PID $EventlogSvchostPID
-}
